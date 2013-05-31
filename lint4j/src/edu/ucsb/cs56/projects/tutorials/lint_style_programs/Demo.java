8577dab44115f94c2fe3ff6ad22d0d4237ae519d
Alex Molina <alexmolina@umail.ucsb.edu>
2013-05-30 18:53:20 -0700

    Added package structure

---
+++ lint4j/src/edu/ucsb/cs56/projects/tutorials/lint_style_programs/Demo.java
@@ -0,0 +1,9 @@
+package edu.ucsb.cs56.projects.tutorials.lint_style_programs;
+
+public class Demo {
+        public static void main (String [] args) {
+	    int a = 5;
+	    a = a;
+	    System.out.println(a);
+}
+}
