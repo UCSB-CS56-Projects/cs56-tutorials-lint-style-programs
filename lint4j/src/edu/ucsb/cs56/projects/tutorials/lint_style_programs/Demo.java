d6b22839500ef6ae88aa85fd4be0ec36d5597b50
Alex Molina <alexmolina@umail.ucsb.edu>
2013-06-01 03:56:22 -0700

    Added more methods that have lint4j problems

--- lint4j/src/edu/ucsb/cs56/projects/tutorials/lint_style_programs/Demo.java
+++ lint4j/src/edu/ucsb/cs56/projects/tutorials/lint_style_programs/Demo.java
@@ -2,18 +2,41 @@ package edu.ucsb.cs56.projects.tutorials.lint_style_programs;
 
 public class Demo {
 
-    void AssignmentNoEffect(){
-	int x = 5;
-	x = x;
+    public void assignmentNoEffect(){
+	int a = 5;
+	a = a; // Statemenet has no effect
     }
-    void AssignmentToBoolean(){
-	int x;
+    public void assignmentToBoolean(){
+	int a;
 	boolean f;
-	if(f = true)
-	    x = 2;
+	if(f = true) // Should be (f == true)
+	    a = 2;
+    }
+    public void Demo(){
+	// Method should not have constructor name
+    }
+    public void equalsArray(){
+	int[] a = {0,1};
+	int[] b = {0,0};
+	int x;
+	if(a.equals(b)){ // equals method on array types tests identity only, consider using Arrays.equal()
+	    x = 1;}
+    }
+    public void equalsString(){
+	String a = "Hi";
+	String b = "There";
+	if(a == b){ // Strings should be compared using equals() method
+	    b = "Hi";}
+    }
+    public void comparingFloatingPoints(){
+	float a = 0.1;
+	float b = 1.1;
+	if(a == b){ // Equality checks with floating point numbers can lead to unexpected behavior
+	    b = 2.2;}
     }
     public static void main (String [] args) {
 	System.out.println("This Compiles and Runs");
+	return 0; // Should not return anything
     }
 
 
